c4eb2d6c85099c732cc8bb6c567765d6bfeb7d78
rohit-bishnoi <rohitbishnoi70@gmail.com>
2022-09-05 09:53:43 +0530

    initial commit

---
+++ Model/RpgClass.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Model;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum RpgClass
+{
+    Knight = 1,
+    Mage = 2,
+    Cleric = 3
+}
